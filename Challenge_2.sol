6792da16d4f3a337fd2edb781f3720448f0440a2
aviin10 <avinkumar013@gmail.com>
2024-06-13 16:17:04 +0530

    Create Challenge_2.sol

---
+++ Challenge_2.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+contract Challenge2 {
+    uint256 public value;
+
+    function setamount(uint256 _value) public{
+        value = _value;
+    }
+
+    function getWei() public view returns (uint256) {
+        return value;
+    }
+
+    function getGwei() public view returns (uint256) {
+        return value / 1 gwei;
+    }
+
+    function getEther() public view returns (uint256) {
+        return value / 1 ether;
+    }
+}
